b2ba96c981d309d9a08d9074ec2a70eca4ca9980
Yiwen Bao <baoyy005@mymail.unisa.edu.au>
2025-04-17 02:54:40 +1000

    Implementing basics of Keycard class and chestcard class
    Implementing the keycard class and chestcard class

--- ConsoleApplication1/ChestCard.h
+++ ConsoleApplication1/ChestCard.h
@@ -1,7 +1,22 @@
 #pragma once
 #include "Card.h"
-class ChestCard :
-    public Card
-{
+class ChestCard : public Card {
+public:
+    ChestCard(int value) : Card(CardType::Chest, value) {}
+
+    void play(Game& game, Player& player) override {
+        // No immediate effect
+        std::cout << "No immediate effect. If banked with a key, draw as many bonus cards from the Discard pile as you moved into your Bank" << std::endl;
+    }
+
+    void willAddToBank(Game& game, Player& player) override;
+
+    void present() const override {
+        std::cout << "Chest(" << value << ")" << std::endl;
+    }
+
+    std::string str() const override {
+        return "Chest(" + std::to_string(value) + ")";
+    }
 };
 
--- ConsoleApplication1/KeyCard.h
+++ ConsoleApplication1/KeyCard.h
@@ -1,7 +1,24 @@
 #pragma once
 #include "Card.h"
-class KeyCard :
-    public Card
-{
+#include <iostream>
+
+class KeyCard : public Card {
+public:
+    KeyCard(int value) : Card(CardType::Key, value) {}
+
+    void play(Game& game, Player& player) override {
+        // No immediate effect
+        std::cout << "No immediate effect. If banked with a chest, draw as many bonus cards from the Discard pile as you moved into your Bank." << std::endl;
+    }
+
+    void willAddToBank(Game& game, Player& player) override;
+
+    void present() const override {
+        std::cout << "Key(" << value << ")" << std::endl;
+    }
+
+    std::string str() const override {
+        return "Key(" + std::to_string(value) + ")";
+    }
 };
 
