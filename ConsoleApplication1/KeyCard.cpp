8fe4fb95cde529f27c897458efbb49cebc107a45
Yiwen Bao <baoyy005@mymail.unisa.edu.au>
2025-04-17 03:13:31 +1000

    Basic Functions of Keycard
    Implementing the basic functions of Keycard

--- ConsoleApplication1/KeyCard.cpp
+++ ConsoleApplication1/KeyCard.cpp
@@ -1,1 +1,11 @@
 #include "KeyCard.h"
+#include "Game.h"
+#include "Player.h"
+
+void KeyCard::play(Game& game, Player& player) {
+    std::cout << "Key has no immediate effect." << std::endl;
+}
+
+void KeyCard::willAddToBank(Game& game, Player& player) {
+    // Do nothing here. Chest handles the combo logic.
+}
